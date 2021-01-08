ff60e8ee0cd2b193edd52245539e4f471639b01c
Shahar <bukrashahar@gmail.com>
2021-01-08 10:36:25 +0200

    prettier

--- src/component/MaxPopTable/maxPopTable.js
+++ src/component/MaxPopTable/maxPopTable.js
@@ -1,37 +1,43 @@
-import React from 'react'
-import './maxPopTable.css'
+import React from "react";
+import "./maxPopTable.css";
 
-export default function MaxPopTable({data}) {
-    return (
-        <table className="tableData">
-        <tbody>
-          <tr>
-            <td>Population</td>
-            {data.map(({population}, i) => <td key={i}>{population.toLocaleString()}</td>)}
-          </tr>
-          <tr>
-            <td>Vehicle name with the largest sum</td>
-            {data.map(({vehiclesName}, i) => <td key={i}>{vehiclesName}</td>)}
-          </tr>
-          <tr>
-            <td>Related home planets and their respective population</td>
-            {data.map(({planets}, i) => (
-                <td key={i}>
-                  {planets.map((planet, j) => <pre key={j}>{planet}</pre>)}
-                </td>
-              )
-            )}
-          </tr>
-          <tr>
-            <td>Related pilot names</td>
-            {data.map((p, i) => (
-                <td key={i}>
-                  {p.pilots.map((pilot, j) => <pre key={j}>{pilot}</pre>)}
-                </td>
-              )
-            )}
-          </tr>
-        </tbody>
-      </table>
-    )
+export default function MaxPopTable({ data }) {
+  return (
+    <table className="tableData">
+      <tbody>
+        <tr>
+          <td>Population</td>
+          {data.map(({ population }, i) => (
+            <td key={i}>{population.toLocaleString()}</td>
+          ))}
+        </tr>
+        <tr>
+          <td>Vehicle name with the largest sum</td>
+          {data.map(({ vehiclesName }, i) => (
+            <td key={i}>{vehiclesName}</td>
+          ))}
+        </tr>
+        <tr>
+          <td>Related home planets and their respective population</td>
+          {data.map(({ planets }, i) => (
+            <td key={i}>
+              {planets.map((planet, j) => (
+                <pre key={j}>{planet}</pre>
+              ))}
+            </td>
+          ))}
+        </tr>
+        <tr>
+          <td>Related pilot names</td>
+          {data.map((p, i) => (
+            <td key={i}>
+              {p.pilots.map((pilot, j) => (
+                <pre key={j}>{pilot}</pre>
+              ))}
+            </td>
+          ))}
+        </tr>
+      </tbody>
+    </table>
+  );
 }
--- src/component/PlanetChartBar/planetChartBar.js
+++ src/component/PlanetChartBar/planetChartBar.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import './planetChartBar.css'
+import React from "react";
+import "./planetChartBar.css";
 
-export default function PlanetChartBar({data}) {
-    return (
-        <table className="tableChart">
-        <tbody>
-          <tr />
-          <tr className="bars">
-            {data.map((chart, i) => {
-              return (
-                <td className="bar" key={i}>
-                  <pre>{chart.population}</pre>
-                  <div
-                    className="barColor"
-                    style={{ height: `${chart.ratio}%` }}
-                  ></div>
-                  <p>{chart.name}</p>
-                </td>
-              );
-            })}
-          </tr>
-        </tbody>
-      </table>
-    )
+export default function PlanetChartBar({ data }) {
+  return (
+    <table className="tableChart">
+      <tbody>
+        <tr />
+        <tr className="bars">
+          {data.map((chart, i) => {
+            return (
+              <td className="bar" key={i}>
+                <pre>{chart.population}</pre>
+                <div
+                  className="barColor"
+                  style={{ height: `${chart.ratio}%` }}
+                ></div>
+                <p>{chart.name}</p>
+              </td>
+            );
+          })}
+        </tr>
+      </tbody>
+    </table>
+  );
 }
